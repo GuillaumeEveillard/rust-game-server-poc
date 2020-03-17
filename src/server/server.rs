0661c1fb2eda232581dc02cee78394bccab2e0e0
geveillard <geveillard@murex.com>
2020-03-17 19:03:12 +0100

    use tokio mutex

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,9 +1,9 @@
-use std::sync::{Arc, Mutex};
-
+use std::sync::{Arc};
+use tokio::sync::Mutex;
 use tokio::sync::mpsc;
 use tonic::{Request, Response, Status, transport::Server};
 
-use game_master::{Action, ActionResult, GameStateRequest, GameStateResponse};
+use game_master::{Action, ActionResult, GameStateRequest, GameStateResponse, LivingBeing};
 use game_master::game_master_server::{GameMaster, GameMasterServer};
 
 pub mod game_master {
@@ -11,12 +11,13 @@ pub mod game_master {
 }
 
 struct StateManager {
-    counter: u32
+    counter: u32,
+    living_beings: Vec<LivingBeing>
 }
 
 impl StateManager {
     fn new() -> StateManager {
-        StateManager{counter: 0}
+        StateManager{counter: 0, living_beings: Vec::new()}
     }
     
     fn inc(&mut self) {
@@ -39,8 +40,8 @@ impl GameMaster for GameServer {
     async fn send_action(&self, request: Request<Action>, ) -> Result<Response<ActionResult>, Status> {
         println!("Got a request from {:?}", request.remote_addr());
         
-        let lock = self.state_manager.lock();
-        let mut state = lock.unwrap();
+        let mut state = self.state_manager.lock().await;
+        // let mut state = lock.unwrap();
         state.inc();
         let c = state.counter;
 
@@ -55,7 +56,12 @@ impl GameMaster for GameServer {
     async fn game_state_streaming(&self, _: Request<GameStateRequest>, ) -> Result<Response<Self::GameStateStreamingStream>, Status> {
         let (mut tx, rx) = mpsc::channel::<Result<GameStateResponse, Status>>(10);
 
+
+        let sm = self.state_manager.clone();
+        
         tokio::spawn(async move {
+            let state = sm.lock();
+            
             let start : u64 = 0;
             for i in start..10 {
                 let to_send = GameStateResponse{counter: i, living_beings: [].to_vec()};
